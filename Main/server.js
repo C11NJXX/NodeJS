6711ec2834887afe63be561912ececaf6801fcc5
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-11 09:38:21 +0800

    Day2(AM) (12-Simple-Router)

--- Main/server.js
+++ Main/server.js
@@ -35,12 +35,44 @@ const PORT = process.env.PORT;
 //     }));
 // });
 
-const server = http.createServer((req,res) => {
-    res.writeHead(200,{
-        'content-type': 'text/plain'
-    });
-    res.end(`Request url: ${req.url}
-Request method: ${req.method}`);
+// const server = http.createServer((req,res) => {
+//     res.writeHead(200,{
+//         'content-type': 'text/plain'
+//     });
+//     res.end(`Request url: ${req.url}
+// Request method: ${req.method}`);
+// });
+
+const server = http.createServer((req, res) => {
+    try {
+        //check if GET method
+        if (req.method === 'GET') {
+            //simple route
+            if (req.url === '/') {
+                res.writeHead(200, {
+                    'content-type': 'text/html'
+                });
+                res.end(`<h1>HomePage</h1>`);
+            } else if (req.url === '/about') {
+                res.writeHead(200, {
+                    'content-type': 'text/html'
+                });
+                res.end(`<h1>About</h1>`);
+            }else {
+                res.writeHead(404, {
+                    'content-type': 'text/html'
+                });
+                res.end(`<h1>404 Not Found</h1>`);
+            }
+        } else {
+            throw new Error('Server Error');
+        }
+    } catch (error) {
+        res.writeHead(500,{
+            'content-type':'text/plain'
+        });
+        res.end('Server Error');
+    }
 });
 
 server.listen(PORT, () => {
