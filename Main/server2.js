de56119f455b1a80f61dd1ca429ebf130bb54394
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-11 13:40:11 +0800

    Day2(AM) (14-Build-Simple-API)

---
+++ Main/server2.js
@@ -0,0 +1,61 @@
+import { createServer } from 'http';
+
+const PORT = process.env.PORT;
+
+//hard code
+const users = [
+    {
+        id: 1,
+        name: "A"
+    },
+    {
+        id: 2,
+        name: "B"
+    },
+    {
+        id: 3,
+        name: "C"
+    }
+];
+
+const server = createServer((req, res) => {
+    try {
+        if (req.method === 'GET') {
+            if (req.url === '/api/users') {
+                res.writeHead(200, {
+                    'content-type': 'application/json'
+                });
+                res.write(JSON.stringify(users));
+                res.end();
+            } else if (req.url.match(/\/api\/users\/([0-9]+)/)) {
+                const id = req.url.split('/')[3];
+                const user = users.find((user) => user.id === parseInt(id));
+                if (user) {
+                    res.writeHead(200, {
+                        'content-type': 'application/json'
+                    });
+                    res.end(JSON.stringify(user));
+                } else {
+                    res.writeHead(404, {
+                        'content-type': 'application/json'
+                    });
+                    res.end(JSON.stringify({ "message": "user not found" }));
+                };
+            }else {
+                throw new Error('404 not found');
+            }
+        }else {
+            throw new Error('method not allowed');
+        }
+    } catch (error) {
+        console.log(error);
+        res.writeHead(500,{
+            'content-type': 'text/plain'
+        });
+        res.end('Server error');
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on PORT:${PORT}`);
+});
